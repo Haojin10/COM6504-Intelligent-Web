49eb5c7c881c28a91ef93f37a27067273688f815
Haojin <carrylee1996@gmail.com>
2021-09-28 21:26:47 +0800

    Default Changelist

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,55 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Data pirates'});
 });
+
+/**
+ *  POST the data about the image.
+ *  parameters in body:
+ *    image: a image url
+ *    author: a author name for image
+ *    title: a title for image
+ *    description: a description for image
+ */
+router.post('/imageRoute', function (req, res, next) {
+  // get random weather for a location
+  let imageData = req.body;
+  console.log(imageData);
+  //Check if the request data is empty
+  if (imageData == null) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(403).json({error: 403, reason: 'The image data is null!'});
+  } else {
+    // send the data back
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(imageData));
+  }
+});
+
+/**
+ *  POST the data about the chat.
+ *  parameters in body:
+ *  roomNumber_image: roomNumber_image, name: name, chatText: chatText
+ *    roomNumber_image: index for retrieve chat from index DB, example: hi, nice to meet you_image url
+ *    name: the user id
+ *    chatText: chat text
+ */
+router.post('/chatRoute', function (req, res, next) {
+  // get random weather for a location
+  let chatData = req.body;
+  console.log(chatData);
+  //Check if the request data is empty
+  if (chatData == null) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(403).json({error: 403, reason: 'The chat data is null!'});
+  } else {
+    // send the data back
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(chatData));
+  }
+});
+
 
 module.exports = router;
--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,9 +1,64 @@
+exports.init = function (io) {
 
-exports.init = function(io) {
-  io.sockets.on('connection', function (socket) {
-    try {
-     // insert here your event
-    } catch (e) {
-    }
-  });
+  const chat = io
+      .of('/chat')
+      .on('connection', function (socket) {
+        try {
+          // insert here your event
+
+          /**
+           * it creates or joins a room
+           */
+          socket.on('create or join', function (room, userId) {
+            socket.join(room);
+            chat.to(room).emit('joined', room, userId);
+          });
+
+          socket.on('chat', function (room, userId, chatText) {
+            chat.to(room).emit('chat', room, userId, chatText);
+          });
+
+          socket.on('disconnect', function () {
+            console.log('someone disconnected');
+          });
+
+          socket.on('draw', function (room, userId, width, height, prevX, prevY, currX, currY, color, thickness) {
+            chat.to(room).emit('draw', room, userId, width, height, prevX, prevY, currX, currY, color, thickness);
+          });
+
+          socket.on('clear', function (room, userId, width, height) {
+            chat.to(room).emit('clear', room, userId, width, height);
+          });
+
+        } catch (e) {
+        }
+      });
+
+  // the news namespace
+  // const pane = io
+  //     .of('/pane')
+  //     .on('connection', function (socket) {
+  //         try {
+  //             /**
+  //              * it creates or joins a room
+  //              */
+  //             socket.on('create or join', function (room, userId) {
+  //                 socket.join(room);
+  //             });
+  //
+  //             socket.on('draw', function (room, userId, width, height, prevX, prevY, currX, currY, color, thickness) {
+  //                 pane.to(room).emit('draw', room, userId, width, height, prevX, prevY, currX, currY, color, thickness);
+  //             });
+  //
+  //             socket.on('clear', function (room, userId, width, height) {
+  //                 pane.to(room).emit('clear', room, userId, width, height);
+  //             });
+  //             //
+  //             // socket.on('disconnect', function () {
+  //             //     console.log('someone disconnected');
+  //             // });
+  //         } catch (e) {
+  //         }
+  //     });
 }
+
